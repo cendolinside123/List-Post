2b955d2b4c41b0045341cadbae2007c09697f7c9
jan <jnsbstn391@gmail.com>
2022-03-12 03:27:27 +0700

    change:
    - improve image caching process

--- List Post/Config/Helper/UIImageView.swift
+++ List Post/Config/Helper/UIImageView.swift
@@ -13,67 +13,85 @@ import Kingfisher
 extension UIImageView {
     func setImage(url: String, times: Int = 3) {
         
-        image = #imageLiteral(resourceName: "shop")
+        image = #imageLiteral(resourceName: "loading")
         
         guard let getURL = URL(string: url) else {
             return
         }
         let imageResource = ImageResource(downloadURL: getURL, cacheKey: url)
         
-        
-        let task = KingfisherManager.shared.retrieveImage(with: imageResource , options: [
-            .processor(DownsamplingImageProcessor(size: self.frame.size)),
-            .scaleFactor(UIScreen.main.scale),
-            .cacheOriginalImage
-        ]) { result in
-            switch result{
-            case .success(_):
-                print("cache success : \(url)")
-            case .failure(let error):
-                print("cache failed : \(error.localizedDescription) url : \(url)")
+        let groupLoad = DispatchGroup()
+        groupLoad.enter()
+        let getFrameSize = self.frame.size
+        DispatchQueue.global().async {
+            KingfisherManager.shared.retrieveImage(with: imageResource , options: [
+                .processor(DownsamplingImageProcessor(size: getFrameSize)),
+                .scaleFactor(UIScreen.main.scale),
+                .cacheOriginalImage
+            ]) { result in
+                switch result{
+                case .success(_):
+                    print("cache success : \(url)")
+                case .failure(let error):
+                    print("cache failed : \(error.localizedDescription) url : \(url)")
+                }
+                groupLoad.leave() // MARK: NOTE FOR REVIEWER: Please give me a feedback is there're crash/bug happend on setImage(url:times:)
             }
         }
         
         // start
-        ImageClassManager.sharedInstance.initialize().retrieveImage(forKey:"\(url)", options: [
-            .transition(.fade(10.0)),
-            .loadDiskFileSynchronously,
-            .forceTransition
-        ]){[weak self] result in
-            switch result {
-                case .success(let value):
-                    DispatchQueue.main.async {
-                        self?.contentMode = .scaleAspectFill
-                        if value.cacheType == .disk{
-                            //self?.image = ThemeManager.shared.songPlaceholder
-                            print("cache local type \(value.cacheType) url:  \(url)")
-                            task?.cancel()
-                            if let cacheImage = value.image{
-                                self?.image = cacheImage
+        
+        groupLoad.notify(queue: .main, execute: {
+            DispatchQueue.global().async {
+                ImageClassManager.sharedInstance.initialize().retrieveImage(forKey:"\(url)", options: [
+                    .transition(.fade(10.0)),
+                    .loadDiskFileSynchronously,
+                    .forceTransition
+                ]){[weak self] result in
+                    switch result {
+                        case .success(let value):
+                            DispatchQueue.main.async {
+                                self?.contentMode = .scaleAspectFill
+                                if value.cacheType == .disk{
+                                    //self?.image = ThemeManager.shared.songPlaceholder
+                                    print("cache local type \(value.cacheType) url:  \(url)")
+                                    
+                                    if let cacheImage = value.image{
+                                        self?.image = cacheImage
+                                    }
+                                    else{
+                                        self?.image = #imageLiteral(resourceName: "loading")
+                                    }
+                                    return
+                                }
+                                else{
+                                    print("cache local type \(value.cacheType) url: \(url)")
+                                    self?.image = #imageLiteral(resourceName: "loading")
+                                    if times > 0 {
+                                        self?.setImage(url: url, times: times - 1)
+                                        return
+                                    }
+                                }
                             }
-                            else{
-                                self?.image = #imageLiteral(resourceName: "shop")
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                            if times > 0 {
+    //                            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5, execute: {
+    //                                self?.image = #imageLiteral(resourceName: "loading")
+    //                            })
+                                self?.image = #imageLiteral(resourceName: "loading")
+                                self?.setImage(url: url, times: times - 1)
+                                return
+                            } else {
+                                print("failed to download song url: \(url)")
                             }
-                            
-                        }
-                        else{
-                            print("cache local type \(value.cacheType) url: \(url)")
-                            self?.image = #imageLiteral(resourceName: "shop")
                         }
                     }
-                case .failure(let error):
-                    print(error.localizedDescription)
-                    if times > 0 {
-                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5, execute: {
-                            self?.image = #imageLiteral(resourceName: "shop")
-                        })
-                        self?.setImage(url: url, times: times - 1)
-                    } else {
-                        print("failed to download song url: \(url)")
-                    }
-                }
             }
-        // end
+            // end
+        })
+        
+        
         
     }
 }
