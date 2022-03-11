ec504aa4dc74746cf33f1799e7bf8012b1f9ca78
jan <jnsbstn391@gmail.com>
2022-03-11 17:49:45 +0700

    add:
    - add implementation of ListProductUIControll for help show and hide loading screen
    - add implementation of ListPostViewModel for diplaing data list of news

--- List Post/Controller/View/Home/HomeViewController.swift
+++ List Post/Controller/View/Home/HomeViewController.swift
@@ -11,21 +11,32 @@ class HomeViewController: UIViewController {
     
     private let tblPost: UITableView = {
         let tabel = UITableView()
-        tabel.backgroundColor = .white
+        tabel.backgroundColor = UIColor(white: 1, alpha: 0)
         tabel.allowsSelection = true
         return tabel
     }()
     
     private let loadingSpinner = UIActivityIndicatorView()
     private let loadingView = UIView()
+    private var uiControll: ListUIGuideHelper?
+    private var viewModel: ListPostVMGuideline?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.navigationItem.title = "Sort Heroes"
+        self.navigationItem.title = "List Post"
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.black]
-        
+        self.view.backgroundColor = UIColor(red: 0.88, green: 0.88, blue: 0.88, alpha: 1)
+        addLayout()
+        addConstraints()
+        uiControll = ListProductUIControll(controller: self)
+        uiControll?.showLoading(completion: nil)
+        let useCase = UseCaseListPost(postDataSource: PostDataSource(), userDataSource: UserDataSource())
+        viewModel = ListPostViewModel(useCase: useCase)
+        setupTabel()
+        bind()
+        viewModel?.loadListPost(try: 3)
     }
     
 
@@ -39,6 +50,16 @@ class HomeViewController: UIViewController {
     }
     */
     
+    private func bind() {
+        viewModel?.fetchError = { [weak self] _ in
+            self?.uiControll?.hideLoading(completion: nil)
+        }
+        viewModel?.postResult = { [weak self] _ in
+            self?.tblPost.reloadData()
+            self?.uiControll?.hideLoading(completion: nil)
+        }
+    }
+    
     private func addLayout() {
         setLoadingView()
         addTabel()
@@ -51,12 +72,16 @@ class HomeViewController: UIViewController {
         
         // MARK: tblMenu constraints
         tblPost.translatesAutoresizingMaskIntoConstraints = false
+        loadingView.translatesAutoresizingMaskIntoConstraints = false
         let hTblPost = "H:|-[tblPost]-|"
-        let vLoadingViewTblPost = "V:|-0-[loadingView]-0-[tblPost]-0-|"
+        let vLoadingViewTblPost = "V:|-[loadingView]-0-[tblPost]-0-|"
         let hLoadingView = "H:|-0-[loadingView]-0-|"
         constraints += NSLayoutConstraint.constraints(withVisualFormat: hTblPost, options: .alignAllTop, metrics: metrix, views: views)
-        constraints += NSLayoutConstraint.constraints(withVisualFormat: vLoadingViewTblPost, options: .alignAllLeading, metrics: metrix, views: views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: vLoadingViewTblPost, options: .alignAllCenterX, metrics: metrix, views: views)
         constraints += NSLayoutConstraint.constraints(withVisualFormat: hLoadingView, options: .alignAllTop, metrics: metrix, views: views)
+        let loadingViewHeight = NSLayoutConstraint(item: loadingView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1/9, constant: 0)
+        loadingViewHeight.identifier = "loadingViewHeight"
+        constraints += [loadingViewHeight]
         
         // MARK: loadingSpinner constraints
         loadingSpinner.translatesAutoresizingMaskIntoConstraints = false
@@ -78,6 +103,16 @@ class HomeViewController: UIViewController {
         loadingView.backgroundColor = .white
         view.addSubview(loadingView)
     }
+    
+    private func setupTabel() {
+        tblPost.delegate = self
+        tblPost.dataSource = self
+        tblPost.register(PostTableViewCell.self, forCellReuseIdentifier: "PostCell")
+        tblPost.rowHeight = UITableView.automaticDimension
+        tblPost.estimatedRowHeight = 250
+        tblPost.tableFooterView = UIView()
+        
+    }
 
 }
 extension HomeViewController {
@@ -88,4 +123,46 @@ extension HomeViewController {
     public func getLoadingSpinner() -> UIActivityIndicatorView {
         return loadingSpinner
     }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel?.listPost.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as? PostTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        if let data = viewModel?.listPost[indexPath.row] {
+            cell.setPost(post: data)
+        } else {
+            return UITableViewCell()
+        }
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = view.backgroundColor
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        
+//        return 350
+//    }
+//    
 }
